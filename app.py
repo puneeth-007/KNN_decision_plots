80d74486505a3d1dcae304fdb8243186dac222b6
puneeth-007 <100269212+puneeth-007@users.noreply.github.com>
2025-03-05 15:40:08 +0530

    Add project files.

---
+++ app.py
@@ -0,0 +1,23 @@
+
+import streamlit as st
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from mlxtend.plotting import plot_decision_regions
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.datasets import make_classification
+from sklearn.datasets import make_blobs
+from sklearn.datasets import make_moons
+from sklearn.datasets import make_circles
+
+
+
+
+
