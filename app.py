c23e2cc7c50f87a3490e606373997d334f8e92a6
puneeth-007 <100269212+puneeth-007@users.noreply.github.com>
2025-03-05 16:03:18 +0530

    sdf

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 from sklearn.datasets import make_classification
 from sklearn.datasets import make_blobs
 from sklearn.datasets import make_moons
@@ -19,7 +18,16 @@ from sklearn.datasets import make_circles
 
 st.title('Decision_surfaces for KNN')
 
-st.dropdown('Dataset',('Blobs','Moons','Circles','Classification'))
+da=st.selectbox('Choose dataset',['make_classification', 'make_blobs', 'make_moons', 'make_circles'])
+k=st.slider(min_value=1, max_value=40,step=1)
+w=st.select_box('Choose weight',['uniform','distance'])
+
+w=st.selectbox('Choose weights',[])
+if da=='make_classification':
+    X, y = make_classification(n_samples=1000, n_features=2, n_redundant=0, n_repeated=0,random_state=20)
+    knn=KNeighborsClassifier(n_neighbors=k,weights=w)
+
+
 
 
 
